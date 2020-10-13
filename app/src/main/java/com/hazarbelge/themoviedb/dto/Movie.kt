9342cbc5e4cc2b034319eb9a52e7d6fc83a437cd
hazarbelge <18401783+hazarbelge@users.noreply.github.com>
2020-10-13 15:12:04 +0300

    Comment lines edited for 2 file.

--- app/src/main/java/com/hazarbelge/themoviedb/dto/Movie.kt
+++ app/src/main/java/com/hazarbelge/themoviedb/dto/Movie.kt
@@ -1,7 +1,9 @@
 package com.hazarbelge.themoviedb.dto
 
-/*We created 2 data class. Movie data class contains informations about the movie and the Movies data class has an immutable variable and its type
-* is a Movie List*/
+/**
+ * We created 2 data class. Movie data class contains informations about the movie and the Movies data class has an immutable variable and its type
+ * is a Movie List
+ */
 data class Movie (val overview: String,
                   val poster_path: String,
                   val release_date: String,
